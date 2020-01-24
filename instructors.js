1dd9206bb00f914ee51bd80575a580280134f540
Carlos Henrique <heinrickcostta@gmail.com>
2020-01-24 15:21:55 -0300

    Adicionado novas views: show.njk, fields.njk, edit.njk e adicionado novos arquivos utils e data.json e outras modiificações foram feitas nos arquivos; instructors.js, styles.css, routes.js, create.njk, index.njk.

--- instructors.js
+++ instructors.js
@@ -1,11 +1,71 @@
+const fs = require('fs');
+const data = require("./data.json");
+const { age } = require('./utils');
+
+exports.show = (req, res)=>{
+    const { id } = req.params;
+
+    const foundInstructor = data.instructors.find((instructor)=>{
+        return id == instructor.id;     
+    })
+
+    if(!foundInstructor) return res.send("instructor not found");
+
+
+    const instructor = {
+        ...foundInstructor,
+        age: age(foundInstructor.birth),
+        services: foundInstructor.services.split(","),
+        created_at: new Intl.DateTimeFormat('pt-BR').format(foundInstructor.created_at),
+    }
+    return res.render("instructors/show",{instructor});
+}
+
 exports.post = (req, res)=>{
-    const keys = Object.keys(req.body)
+    const keys = Object.keys(req.body);
  
     for(key of keys) {
-        if (req.body[key]== ""){
-            return res.send("Por favor preencha todos os campos")
+        if (req.body[key] == ""){
+            return res.send("Por favor preencha todos os campos");
         }
     }
-    return res.send(req.body)
- }
+
+    let {avatar_url, name, birth, gender, services }=req.body;
+
+    const id = Number(data.instructors.length + 1);
+    birth = Date.parse(req.body.birth);
+    const created_at = Date.now();
+    
  
+    data.instructors.push({
+        id,
+        avatar_url,
+        name,
+        gender,
+        birth,
+        services,
+        created_at,
+    });
+    
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2),(err)=>{
+        if(err){
+            return res.send(err);
+        }else{
+            return res.redirect("/instructors");
+        }
+    })
+
+}
+
+exports.edit = (req, res)=>{
+    const { id } = req.params;
+
+    const foundInstructor = data.instructors.find((instructor)=>{
+        return id == instructor.id;     
+    })
+
+    if(!foundInstructor) return res.send("instructor not found");
+
+    return res.render('instructors/edit',{instructor: foundInstructor});
+}
